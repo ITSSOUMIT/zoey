4060e8d6e6fe5610f5b87c7c31e3fcd82edc5b0b
Soumit Das <its.soumit.das@gmail.com>
2024-09-25 03:41:57 +0530

    configurations corrected

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="zoey",
     version="0.1",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'click',
     ],
--- zoey/scopes/generators.py
+++ zoey/scopes/generators.py
@@ -1,7 +1,35 @@
 from pathlib import Path
 import click
+import shutil
+import os
+
+
+def clear_public_directory():
+    public_directory = Path('public')
+    if public_directory.exists():
+        shutil.rmtree(public_directory)
+        public_directory.mkdir()
+    else:
+        public_directory.mkdir()
+
+
+def traverse_and_convert():
+    src_directory = Path('src')
+    public_directory = Path('public')
+    for root, dirs, files in os.walk(src_directory):
+        for file in files:
+            src_file = Path(root) / file
+            public_file = public_directory / src_file.relative_to(src_directory)
+            public_file.parent.mkdir(parents=True, exist_ok=True)
+            shutil.copy(src_file, public_file)
 
 
 @click.command()
 def site():
-    click.echo('Generating an optimized build...')
+    public_directory = Path('public')
+    click.echo(click.style('Generating an optimized build...', fg='yellow'))
+    click.echo(click.style('Emptying the public directory...', fg='red'))
+    clear_public_directory()
+    click.echo(click.style('Converting source files to HTML...', fg='yellow'))
+    traverse_and_convert()
+    click.echo(click.style(f'Your site is generated successfully inside {public_directory} directory', fg='green'))
