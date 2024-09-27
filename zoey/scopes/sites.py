83b29936ad35a16f9bc82f2a800fac38503fa04a
Soumit Das <its.soumit.das@gmail.com>
2024-09-27 23:36:40 +0530

    v0.3

--- zoey/scopes/sites.py
+++ zoey/scopes/sites.py
@@ -11,6 +11,8 @@ def site(name):
         template_path = Path(pkg_resources.resource_filename('zoey', 'templates/project_base'))
         project_path = Path(name)
         shutil.copytree(template_path, project_path)
+        for pycache_dir in project_path.glob('**/__pycache__'):
+            shutil.rmtree(pycache_dir)
         click.echo(f"Created new project: {name}")
     except Exception as e:
         click.echo(f"Error creating project: {str(e)}")
